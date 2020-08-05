c00a56408af2aa7b78c111d3ece091fb7bd0d516
vlad <vladkalyuzhnyu@gmail.com>
2020-08-05 12:31:59 +0300

    changes

--- app/src/main/java/defpackage/MyApp.kt
+++ app/src/main/java/defpackage/MyApp.kt
@@ -1,6 +1,18 @@
 package defpackage
 
 import android.app.Application
+import com.jakewharton.threetenabp.AndroidThreeTen
+import org.kodein.di.android.BuildConfig
+import timber.log.Timber
 
 @Suppress("unused")
-class MyApp : Application()
+class MyApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+        AndroidThreeTen.init(this)
+    }
+}
---
+++ app/src/main/java/defpackage/local/Preferences.kt
@@ -0,0 +1,9 @@
+package defpackage.local
+
+import android.content.Context
+import com.chibatching.kotpref.KotprefModel
+
+class Preferences(context: Context) : KotprefModel(context) {
+
+    override val kotprefName = "${context.packageName}_preferences"
+}
