9bffd3c0a656d634d9e13f0b57187d652869e91e
vlad <vladkalyuzhnyu@gmail.com>
2020-08-05 12:12:46 +0300

    changes

--- app/src/main/java/defpackage/extension/ActivityManager.kt
+++ app/src/main/java/defpackage/extension/ActivityManager.kt
@@ -1,10 +1,11 @@
-@file:Suppress("unused", "DEPRECATION")
+@file:Suppress("unused")
 
 package defpackage.extension
 
 import android.app.ActivityManager
 import android.app.Service
 
+@Suppress("DEPRECATION")
 inline fun <reified T : Service> ActivityManager.isRunning(): Boolean {
     for (service in getRunningServices(Int.MAX_VALUE)) {
         if (T::class.java.name == service.service.className) {
